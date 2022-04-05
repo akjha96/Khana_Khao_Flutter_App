59fb10d56f6317e2fe0126367a9eee78ef8ca3f7
Anand Jha <akjha96@gmail.com>
2022-04-05 16:26:21 +0530

    Connected CategoryScreen to myApp()

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:khana_khao/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Khana Khao')),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                title: categoryData.title, color: categoryData.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,35 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Khana Khao'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Navigation Time!',
-            ),
-          ],
-        ),
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
