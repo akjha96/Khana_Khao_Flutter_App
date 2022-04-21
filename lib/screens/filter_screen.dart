3fa9723a5d191e83308d6a40593240bd57370045
Anand Jha <akjha96@gmail.com>
2022-04-21 19:15:08 +0530

    Added Scaffold to filtersScreen & fixed infinite drawer route stack

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   const FiltersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filter'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
         child: Text('Filters!'),
       ),
     );
